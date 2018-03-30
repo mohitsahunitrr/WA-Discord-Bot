cfb74f9c45477097bfac3af3c415ffe069e8cbde
KipperedSnack <20378331+KipperedSnack@users.noreply.github.com>
2018-03-30 17:43:59 -0500

    Testing...

--- bot.js
+++ bot.js
@@ -1276,9 +1276,9 @@ const commands = {
 			exec("rm -rf /storage/WA-Bot/assets/public/music/*.mp3");
 			exec("cp -rf /storage/WA-Bot/assets/public/music-orig/*.mp3 /storage/WA-Bot/assets/public/music/.");
 			exec("mv /storage/listen.m3u.orig /storage/listen.m3u");
- 			msg.channel.send("<:main_computer:420575980198035456> :headphones: :fast_forward:  `[Main Computer] Radio @ WA.Net# Wiping radio queue...`");
+ 			msg.channel.send("<:main_computer:420575980198035456> :headphones: :wastebasket:  `[Main Computer] Radio @ WA.Net# Wiping radio queue...`");
 			radioQueue("422898611106480139");
- 			msg.channel.send("<:main_computer:420575980198035456> :headphones: :fast_forward:  `[Main Computer] Radio @ WA.Net# Radio queue wipe completed!`");
+ 			msg.channel.send("<:main_computer:420575980198035456> :headphones: ::white_check_mark:  `[Main Computer] Radio @ WA.Net# Radio queue wipe completed!`");
 			exec("pkill -10 ices && pkill -1 ices");			
 			break;			
 		case "play":
