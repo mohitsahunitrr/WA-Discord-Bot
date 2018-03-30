e7ef38f8a56ea1f1f702723e5498de8cfcc54ff5
KipperedSnack <20378331+KipperedSnack@users.noreply.github.com>
2018-03-30 17:11:08 -0500

    Testing...

--- bot.js
+++ bot.js
@@ -1404,7 +1404,7 @@ const commands = {
 			var youTube = new YouTube();
 			youTube.setKey(api_youtube_data);
 			var prettySearchTerm = searchRaw;
-			var searchTerm = msg.content.split(' ')[3].replace(/ /g, '+');
+			var searchTerm = searchRaw.replace(/ /g, '%20');
 			youTube.search(searchTerm, 1, function(error, result) {
 				if (error) {
 					console.log(error);
