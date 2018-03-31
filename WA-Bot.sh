d593bd6982c6ecff50d9e40c013aa47894ac3b5d
KipperedSnack <20378331+KipperedSnack@users.noreply.github.com>
2018-03-30 19:48:00 -0500

    Testing...

--- WA-Bot.sh
+++ WA-Bot.sh
@@ -2,6 +2,8 @@
 cd "$(dirname "$0")"
 if ! screen -list | grep -q "WA-Bot"; then
 	cd /storage/WA-Bot/
+	killall ices;
+	/storage/ices-start.sh;
 	git stash;
 	git pull;
     screen -LdmS WA-Bot node /storage/WA-Bot/bot.js	
